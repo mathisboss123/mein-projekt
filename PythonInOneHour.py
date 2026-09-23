8ebc32a43127de5d8613f1bdf73ff9adc2665ad9
Mathisboss_123 <mathisbeat.huber@uzh.ch>
2026-09-23 21:21:51 +0200

    testtest

--- PythonInOneHour.py
+++ PythonInOneHour.py
@@ -1,4 +1,23 @@
-print ("hello")
+#print ("hello")
+
+#age = 20
+#print(age)
+
+#first_name = "Mathis"
+#print(first_name)
 
 
+#its_raining = True  
 
+#name = input("What is your name? ")
+#print("Hello " + name)
+#   
+#input = input("what is your birth year? ")
+#age = 2024 - int(input)
+#print("You are " + str(age) + " years old.")
+#my_age = 20
+#print("I am " + str(my_age) + " years old.")
+
+#birth_year = input("What is your birth year? ")
+#age = 2026 - int(birth_year)
+#print("You are " + str(age) + " years old.")
