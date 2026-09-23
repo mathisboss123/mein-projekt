782980eef452e373c06f96e502f8b9180944a3ad
Mathisboss_123 <mathisbeat.huber@uzh.ch>
2026-09-23 21:37:34 +0200

    commit 4

--- PythonInOneHour.py
+++ PythonInOneHour.py
@@ -21,3 +21,8 @@
 #birth_year = input("What is your birth year? ")
 #age = 2026 - int(birth_year)
 #print("You are " + str(age) + " years old.")
+
+#first = input("first: ")
+#second = input("second: ")
+#sum = float(first) + float(second)
+#print("Sum: " + str(sum))         
